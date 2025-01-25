827e0a85ba372be2e6817c4579bbbafe1539639a
July Bernard <julybernard1993@gmail.com>
2025-01-24 19:48:37 -0500

    removed the depends_on option from public_access_block

--- s3/public_access_block_s3.tf
+++ s3/public_access_block_s3.tf
@@ -4,6 +4,5 @@ resource "aws_s3_bucket_public_access_block" "allow-public-access-s3" {
 	ignore_public_acls      = false
 	block_public_policy     = false
 	restrict_public_buckets = false
-	depends_on = [aws_s3_bucket.amzn_batch_id_012025]
+	
   }
-  ###depends_on was added for some test and could be deleted at any moment after development is completed
